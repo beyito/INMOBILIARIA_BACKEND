21b6feda4febd732ea01a130509ab2acc2c8bae0
Oliver <venturavalverdeo@gmail.com>
2025-10-09 00:23:02 -0400

    WIP

--- inmobiliaria/settings.py
+++ inmobiliaria/settings.py
@@ -90,31 +90,41 @@ CHANNEL_LAYERS = {
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'inmobiliaria',     # nombre de tu base de datos
-        'USER': 'postgres',
-        'PASSWORD': 'KevinAntonio', # tu contraseña de PostgreSQL
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
+
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'inmobiliaria',     # nombre de tu base de datos
+#        'USER': 'postgres',
+#        'PASSWORD': 'KevinAntonio', # tu contraseña de PostgreSQL
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+#}
+
  
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT', cast=int),
-        'OPTIONS': {
-            'sslmode': 'require',  # Deshabilita el uso de SSL
-        },
-    }    
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': config('DB_NAME'),
+#        'USER': config('DB_USER'),
+#        'PASSWORD': config('DB_PASSWORD'),
+#        'HOST': config('DB_HOST'),
+#        'PORT': config('DB_PORT', cast=int),
+#        'OPTIONS': {
+#            'sslmode': 'require',  # Deshabilita el uso de SSL
+#        },
+#    }    
+#}
 
 
 # DATABASES = {
