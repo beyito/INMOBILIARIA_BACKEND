51318b45896645caf35a2d9e366618ab0239fcb0
Benjamin Herbas Miranda <cobubenjamin898@gmail.com>
2025-10-09 02:14:41 -0400

    .

--- inmobiliaria/settings.py
+++ inmobiliaria/settings.py
@@ -90,19 +90,19 @@ CHANNEL_LAYERS = {
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'inmobiliaria',     # nombre de tu base de datos
-        'USER': 'postgres',
-        'PASSWORD': 'KevinAntonio', # tu contraseña de PostgreSQL
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'inmobiliaria',     # nombre de tu base de datos
+#         'USER': 'postgres',
+#         'PASSWORD': 'KevinAntonio', # tu contraseña de PostgreSQL
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
  
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('DB_NAME'),
@@ -115,7 +115,7 @@ DATABASES = {
         },
     }    
 }
-"""
+
 
 # DATABASES = {
 #     'default': {
