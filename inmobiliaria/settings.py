d6a7a60a331607d9d8ae8148c72f5a737c62b19b
NohemiChoqueED2 <universidad.nohemi@gmail.com>
2025-11-05 14:45:21 -0400

    sp4

--- inmobiliaria/settings.py
+++ inmobiliaria/settings.py
@@ -12,9 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+CONTRATOS_URL = '/contratos/'
+CONTRATOS_ROOT = os.path.join(BASE_DIR, 'contratos')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
