92f5df0ae9422bbb83ac69b16a2b7fbe024a9a94
NohemiChoqueED2 <universidad.nohemi@gmail.com>
2025-11-22 18:24:37 -0400

    feat(core): agregar configuración de ventas y ruta del webhook Stripe en settings y urls

--- inmobiliaria/settings.py
+++ inmobiliaria/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'desempeno',
     'alertas',
     'reportes',
+    'ventas',
 ]
 
 MIDDLEWARE = [
@@ -96,16 +97,17 @@ CHANNEL_LAYERS = {
 
  #Database
 #https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'inmobiliaria',     # nombre de tu base de datos
-#         'USER': 'postgres',
-#         'PASSWORD': 'KevinAntonio', # tu contraseña de PostgreSQL
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': config("DB_ENGINE", default="django.db.backends.postgresql"),
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST", default="localhost"),
+        'PORT': config("DB_PORT", default="5432"),
+    }
+}
+
 
 # DATABASES = {
 #     'default': {
@@ -195,6 +197,15 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 EMAIL_TIMEOUT = 30
 
 ALERTAS_AUTO_DELETE_INVALID_DEVICE = True # o False (por defecto)
+
+# Stripe Keys
+STRIPE_SECRET_KEY = config("STRIPE_SECRET_KEY", default="")
+STRIPE_PUBLIC_KEY = config("STRIPE_PUBLIC_KEY", default="")
+FRONTEND_SUCCESS_URL = config("FRONTEND_SUCCESS_URL", default="http://localhost:3000/success")
+FRONTEND_CANCEL_URL = config("FRONTEND_CANCEL_URL", default="http://localhost:3000/cancel")
+
+STRIPE_WEBHOOK_SECRET = config("STRIPE_WEBHOOK_SECRET", default="")
+
 
 #pip freeze > requirements.txt PARA GENERAR LISTA DE REQUIRIMIENTOS
 #pip install -r requirements.txt PARA INSTALAR LOS REQUIRIMIENTOS
